84b45d2d04d3cf3ea1f49597c334ac7462c2aeda
lorran <lorransilva059@gmail.com>
2024-10-10 17:01:55 -0300

    declaracao de variaveis

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          alteracao git
-        </a>
-      </header>
-    </div>
+  const [email, setEmail] = useState("")
+  const [senha, setSenha] = useState("")
+
+  return(
+    <form onSubmit={funcao}>
+      
+
+    </form>
   );
 }
 
